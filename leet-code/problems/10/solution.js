b580521c2e04d73e1a8bc8a9e3b1e60ef4593410
guodp <guodp9u0@gmail.com>
2018-12-13 16:30:17 +0800

    #10

--- leet-code/problems/10/solution.js
+++ leet-code/problems/10/solution.js
@@ -6,78 +6,80 @@
 var isMatch = function (s, p) {
   let m = s.length,
     n = p.length;
-  let dp = Array(m + 1);
+  let f = [];
   for (let i = 0; i < m + 1; i++) {
-    dp[i] = [];
+    f[i] = [];
     for (let j = 0; j < n + 1; j++) {
-      dp[i][j] = false;
+      f[i][j] = false;
     }
   }
-  dp[0][0] = true;
+  f[0][0] = true;
   for (let i = 0; i <= m; i++) {
     for (let j = 1; j <= n; j++) {
       let s_1 = s[i - 1];
-      if (p[j - 1] === '*') {
+      let p_1 = p[j - 1];
+      if (p_1 === '*') {
         let p_2 = p[j - 2];
-        dp[i][j] = dp[i][j - 2] || (i > 0 && dp[i - 1][j] && (s_1 === p_2 || p_2 === '.'));
+        f[i][j] = f[i][j - 2] || (i > 0 && f[i - 1][j] && (s_1 === p_2 || p_2 === '.'));
       } else {
-        let p_1 = p[j - 1];
-        dp[i][j] = i > 0 && dp[i - 1][j - 1] && (s_1 === p_1 || p_1 === '.');
+        f[i][j] = i > 0 && f[i - 1][j - 1] && (s_1 === p_1 || p_1 === '.');
       }
     }
   }
-  return dp[m][n];
+  // DEBUG: OUTPUT dp
+  // console.log(dp);
+  return f[m][n];
 };
 
 //TEST:
 let s, p;
 
-s = "aabcbcbcaccbcaabc"
-p = ".*a*aa*.*b*.c*.*a*"
-console.log(isMatch(s, p));
+// s = "aabcbcbcaccbcaabc"
+// p = ".*a*aa*.*b*.c*.*a*"
+// console.log(isMatch(s, p));
 
-s = ""
-p = "c*c*"
-console.log(isMatch(s, p));
+// s = ""
+// p = "c*c*"
+// console.log(isMatch(s, p));
 
 s = ""
 p = ".*"
 console.log(isMatch(s, p));
 
-s = ""
-p = "."
-console.log(isMatch(s, p));
+// s = ""
+// p = "."
+// console.log(isMatch(s, p));
 
-s = ""
-p = "c*"
-console.log(isMatch(s, p));
+// s = ""
+// p = "c*"
+// console.log(isMatch(s, p));
 
-s = "aa"
-p = "a"
-console.log(isMatch(s, p));
+// s = "aa"
+// p = "a"
+// console.log(isMatch(s, p));
 
-s = "aa"
-p = "a*"
-console.log(isMatch(s, p));
-
-
-s = "ab"
-p = ".*"
-console.log(isMatch(s, p));
-
-s = "aab"
-p = "c*a*b"
-console.log(isMatch(s, p));
-
-s = "mississippi"
-p = "mis*is*p*."
-console.log(isMatch(s, p));
+// s = "aa"
+// p = "a*"
+// console.log(isMatch(s, p));
 
 
-s = "ab"
-p = ".*c"
-console.log(isMatch(s, p));
+// s = "ab"
+// p = ".*"
+// console.log(isMatch(s, p));
 
-s = "aaca"
-p = "ab*a*c*a"
-console.log(isMatch(s, p));
+// s = "aab"
+// p = "c*a*b"
+// console.log(isMatch(s, p));
+
+// s = "mississippi"
+// p = "mis*is*p*."
+// console.log(isMatch(s, p));
+
+
+// s = "ab"
+// p = ".*c"
+// console.log(isMatch(s, p));
+
+// s = "aaca"
+// p = "ab*a*c*a"
+// console.log(isMatch(s, p));

614803333999fdc011f899a4580798a52c7c9e3b
guodp <guodp9u0@gmail.com>
2018-12-21 17:48:31 +0800

    #119

--- leetcode/problems/100-199/119/solution.js
+++ leetcode/problems/100-199/119/solution.js
@@ -5,15 +5,18 @@
 var getRow = function (rowIndex) {
   let f = [1];
   for (let i = 1; i < rowIndex + 1; i++) {
-    for (let j = i; j >= 1; j--) {
-      f[j] = (f[j] || 0) + (f[j - 1] || 0);
+    f[i] = 1;
+    for (let j = i - 1; j >= 1; j--) {
+      f[j] += f[j - 1];
     }
   }
   return f;
 };
 
 /**
+/**
 // TEST:
 
 console.log(getRow(5));
+*/
 */

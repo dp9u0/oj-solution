adc263bfc857b5a98eccbfb01ed6a532b7147d3e
guodp <guodp9u0@gmail.com>
2018-12-21 09:24:15 +0800

    update script

--- leet-code/scripts/common.js
+++ leet-code/scripts/common.js
@@ -1,6 +1,6 @@
 let fs = require('fs');
 
-SOLUTION_JS_PATH = `./solving/solution.js`;
+SOLUTION_JS_PATH = `./solution.js`;
 
 module.exports.SOLUTION_JS_PATH = SOLUTION_JS_PATH;
 
--- leet-code/scripts/start.js
+++ leet-code/scripts/start.js
@@ -1,11 +1,14 @@
 let fs = require('fs');
 let common = require('./common')
 let exec = require('child_process').exec;
-
-let problem = process.argv[2] || '1';
+let problem = process.argv[2] || ~~(Math.random() * 960);
 let solutionjsPath = common.SOLUTION_JS_PATH;
 let jsPath = common.getJsPath(problem)
 let markdownPath = common.getMdPath(problem)
+
+fs.mkdirSync('./solving', {
+  recursive: true
+});
 
 let cmdStr = `leetcode show -g ${problem} -o solving`;
 if (common.exists(problem)) {

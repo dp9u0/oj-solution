53e87b5e8963c90de2d6e91899a6266e833e48aa
guodp <guodp9u0@gmail.com>
2020-04-27 08:31:43 +0800

    #84

--- leetcode/problems/000-099/84/solution.js
+++ leetcode/problems/000-099/84/solution.js
@@ -4,36 +4,36 @@
  */
 var largestRectangleArea = function (heights) {
   if (heights === null || heights.length === 0) {
-    return 0;
+      return 0;
   }
   let stack = [];
   let max = 0;
   let counter = 0;
   while (counter < heights.length) {
-    // push index to stack when the current height is larger than the previous one
-    if (stack.length === 0 || heights[counter] >= heights[stack[stack.length - 1]]) {
-      stack.push(counter);
-      counter++;
-    } else {
-      // calculate max value when the current height is less than the previous one
-      let topStackElement = stack.pop();
-      let itemHeight = heights[topStackElement];
-      let wide = stack.length === 0
-        ? counter
-        : counter - stack[stack.length - 1] - 1;
-      let area = itemHeight * wide;
-      max = Math.max(area, max);
-    }
+      // push index to stack when the current height is larger than the previous one
+      if (stack.length === 0 || heights[counter] >= heights[stack[stack.length - 1]]) {
+          stack.push(counter);
+          counter++;
+      } else {
+          // calculate max value when the current height is less than the previous one
+          let top = stack.pop();
+          let height = heights[top];
+          let wide = stack.length === 0
+              ? counter
+              : counter - stack[stack.length - 1] - 1;
+          let area = height * wide;
+          max = Math.max(area, max);
+      }
   }
   // for only ladder type (increasing-only) histograms.
   while (stack.length > 0) {
-    let topStackElement = stack.pop();
-    let itemHeight = heights[topStackElement];
-    let wide = stack.length === 0
-      ? counter
-      : counter - stack[stack.length - 1] - 1;
-    let area = itemHeight * wide;
-    max = Math.max(area, max);
+      let top = stack.pop();
+      let height = heights[top];
+      let wide = stack.length === 0
+          ? counter
+          : counter - stack[stack.length - 1] - 1;
+      let area = height * wide;
+      max = Math.max(area, max);
   }
   return max;
 };

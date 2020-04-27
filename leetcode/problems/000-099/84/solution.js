4f378aa1e6d4b81e56090813b6a7e50ae87e2897
guodp <guodp9u0@gmail.com>
2020-04-27 13:44:51 +0800

    #84

--- leetcode/problems/000-099/84/solution.js
+++ leetcode/problems/000-099/84/solution.js
@@ -2,39 +2,24 @@
  * @param {number[]} heights
  * @return {number}
  */
-var largestRectangleArea = function (heights) {
-    if (heights === null || heights.length === 0) {
-        return 0;
-    }
-    let stack = [-1]; // 哨兵
+const largestRectangleArea = (heights) => {
+    const stack = [];
     let max = 0;
+    heights = [0, ...heights, 0]; // 哨兵
     let counter = 0;
     while (counter < heights.length) {
-        let currentHeight = heights[counter];
-        if (stack.length === 1 || currentHeight >= heights[stack[stack.length - 1]]) {
-            // 当前高度比前一个高度高 说明可以与前一个高度组成更大的面积
-            stack.push(counter);
-            counter++;
-        } else {
-            // 当前高度比前一个高度低 需要计算前一个高度的面积
-            let top = stack.pop();
-            let height = heights[top];
-            let width = counter - stack[stack.length - 1] - 1;
-            let area = height * width;
-            max = Math.max(area, max);
+        while (stack.length && heights[stack[stack.length - 1]] > heights[counter]) {
+            const heightIndex = stack.pop();
+            const height = heights[heightIndex];
+            const width = counter - stack[stack.length - 1] - 1;
+            const area = height * width;
+            max = Math.max(max, area)
         }
-    }
-    // 由于升序序列 可能导致还有元素没有被处理
-    while (stack.length > 1) {
-        let top = stack.pop();
-        let height = heights[top];
-        let width = counter - stack[stack.length - 1] - 1;
-        let area = height * width;
-        max = Math.max(area, max);
+        stack.push(counter);
+        counter++;
     }
     return max;
-};
-
+}
 
 // TEST:
 console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]))

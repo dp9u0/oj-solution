019c57f4384d36452192847d8bd937ee38d7eafc
guodp <guodp9u0@gmail.com>
2020-04-30 14:04:51 +0800

    script

--- leetcode/scripts/ok.js
+++ leetcode/scripts/ok.js
@@ -36,10 +36,10 @@ fs.copyFileSync(common.SOLUTION_MD_PATH, markdownPathTarget);
 
 // clean up
 common.removeCurrent();
-if (fs.existsSync(jsPathSolving)) { fs.unlinkSync(jsPathSolving); }
-if (fs.existsSync(markdownPathSolving)) { fs.unlinkSync(markdownPathSolving); }
-if (fs.existsSync(common.SOLUTION_JS_PATH)) { fs.unlinkSync(common.SOLUTION_JS_PATH); }
-if (fs.existsSync(common.SOLUTION_MD_PATH)) { fs.unlinkSync(common.SOLUTION_MD_PATH); }
+// if (fs.existsSync(jsPathSolving)) { fs.unlinkSync(jsPathSolving); }
+// if (fs.existsSync(markdownPathSolving)) { fs.unlinkSync(markdownPathSolving); }
+// if (fs.existsSync(common.SOLUTION_JS_PATH)) { fs.unlinkSync(common.SOLUTION_JS_PATH); }
+// if (fs.existsSync(common.SOLUTION_MD_PATH)) { fs.unlinkSync(common.SOLUTION_MD_PATH); }
 
 // update readme
 common.updateReadme({ problem, title, level, topics, status: ':o:', remark });

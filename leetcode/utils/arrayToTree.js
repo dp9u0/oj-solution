4fd582f6a5ac76f8edd66d7f6106cd6c09286325
guodp <guodp9u0@gmail.com>
2021-01-20 17:33:39 +0800

    utils

--- leetcode/utils/arrayToTree.js
+++ leetcode/utils/arrayToTree.js
@@ -3,38 +3,23 @@
  * @return {TreeNode}
  */
 function arrayToTree(array) {
-  if (!array.length || !array[0]) {
+  if (!array.length || array[0] === null) {
     return null;
   }
-  let root = {
-    val: array.shift(),
-    left: null,
-    right: null
-  }
+  let root = { val: array.shift(), left: null, right: null }
   let q = [root];
   while (array.length) {
     let node = q.shift();
-    if (!node) {
-      throw 'unvalid array'
-    }
-    if (array[0]) {
-      node.left = {
-        val: array[0],
-        left: null,
-        right: null
-      }
+    let val = array.shift();
+    if (val || val === 0) {
+      node.left = { val, left: null, right: null }
       q.push(node.left);
     }
-    array.shift();
-    if (array[0]) {
-      node.right = {
-        val: array[0],
-        left: null,
-        right: null
-      }
+    val = array.shift();
+    if (val || val === 0) {
+      node.right = { val, left: null, right: null }
       q.push(node.right);
     }
-    array.shift();
   }
   return root;
 }

476d8e4810c6b4f6b9bcc668ad353b7c4c49aaf1
guodp <guodp9u0@gmail.com>
2026-08-29 20:26:44 +0800

    feat: resolve leetcode 2006

--- problems/2000-2099/2006/solution.js
+++ problems/2000-2099/2006/solution.js
@@ -11,17 +11,21 @@
  * @return {number}
  */
 var countKDifference = function(nums, k) {
-    const count = new Map();
-    let result = 0;
-    for (const num of nums) {
-        result += (count.get(num - k) || 0) + (count.get(num + k) || 0);
-        count.set(num, (count.get(num) || 0) + 1);
-    }
-    return result;
+  const count = Array(101).fill(0);
+  let ans = 0;
+  for (const x of nums) {
+    ans += (x - k >= 1 ? count[x - k] : 0) + (x + k <= 100 ? count[x + k] : 0);
+    count[x]++;
+  }
+  return ans;
 };
 // @lc code=end
 
 // TEST:
-console.log(countKDifference([1,2,2,1], 1));      // 4
-console.log(countKDifference([1,3], 3));           // 0
-console.log(countKDifference([3,2,1,5,4], 2));    // 3
+console.log(countKDifference([1, 2, 2, 1], 1) === 4);
+console.log(countKDifference([1, 3], 3) === 0);
+console.log(countKDifference([3, 2, 1, 5, 4], 2) === 3);
+console.log(countKDifference([1], 1) === 0);
+console.log(countKDifference([2, 2], 1) === 0);
+console.log(countKDifference([1, 2, 3, 4, 5], 1) === 4);
+console.log(countKDifference([10, 1, 11], 1) === 1);

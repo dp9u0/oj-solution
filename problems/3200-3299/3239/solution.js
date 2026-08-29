a1beeb164fabf31dcbc0bca7bd8764d997e76d96
guodp <guodp9u0@gmail.com>
2026-08-29 20:41:30 +0800

    feat: resolve leetcode 3239

--- problems/3200-3299/3239/solution.js
+++ problems/3200-3299/3239/solution.js
@@ -10,29 +10,29 @@
  * @return {number}
  */
 var minFlips = function(grid) {
-    const m = grid.length, n = grid[0].length;
-    let rowFlips = 0, colFlips = 0;
-
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < Math.floor(n / 2); j++) {
-            if (grid[i][j] !== grid[i][n - 1 - j]) rowFlips++;
-        }
+  const m = grid.length;
+  const n = grid[0].length;
+  let rowCost = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n >> 1; j++) {
+      if (grid[i][j] !== grid[i][n - 1 - j]) rowCost++;
     }
-
-    for (let j = 0; j < n; j++) {
-        for (let i = 0; i < Math.floor(m / 2); i++) {
-            if (grid[i][j] !== grid[m - 1 - i][j]) colFlips++;
-        }
+  }
+  let colCost = 0;
+  for (let j = 0; j < n; j++) {
+    for (let i = 0; i < m >> 1; i++) {
+      if (grid[i][j] !== grid[m - 1 - i][j]) colCost++;
     }
-
-    return Math.min(rowFlips, colFlips);
+  }
+  return Math.min(rowCost, colCost);
 };
 // @lc code=end
 
 // TEST:
-console.log(minFlips([[1,0,0],[0,0,0],[0,0,1]])); // 2
-console.log(minFlips([[0,1],[0,1],[0,0]])); // 1
-console.log(minFlips([[1],[0]])); // 0
-console.log(minFlips([[1,1],[1,1]])); // 0
-console.log(minFlips([[0,1],[1,0]])); // 1
-console.log(minFlips([[1,0,1],[0,1,0]])); // 0
+console.log(minFlips([[1, 0, 0], [0, 0, 0], [0, 0, 1]]) === 2);
+console.log(minFlips([[0, 1], [0, 1], [0, 0]]) === 1);
+console.log(minFlips([[1], [0]]) === 0);
+console.log(minFlips([[1, 1], [1, 1]]) === 0);
+console.log(minFlips([[1, 0], [0, 1]]) === 2);
+console.log(minFlips([[1, 1, 1, 0]]) === 0);
+console.log(minFlips([[1, 0], [1, 0]]) === 0);
